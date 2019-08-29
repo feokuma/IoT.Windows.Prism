0d775c5a229d5e7e68faef8eb56258ba508feb8b
Fernando Okuma <feokuma@gmail.com>
2019-08-29 00:24:43 -0300

    upload

---
+++ IoT.Windows.Prism/Models/GifInfo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace gif.load
+{
+    public class GifInfo
+    {
+        #region Filedsprivate 
+        FileInfo fileInfo;
+        private List<byte[]> frames;
+        private Size size;
+        private bool animated;
+        private bool loop;
+        private TimeSpan animationDuration;
+        #endregion
+
+        #region Properties
+
+        public FileInfo FileInfo => fileInfo; 
+
+        public List<byte[]> Frames => frames;
+
+        public Size Size => size;
+
+        public bool Animated => animated;
+
+        public bool Loop => loop;
+
+        public TimeSpan AnimationDuration => animationDuration;
+
+        #endregion
+
+
+        #region Constructors
+
+        public GifInfo(String filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                using (var image = Image.FromFile(filePath))
+                {
+                    this.size = new Size(image.Width, image.Height);
+
+                    if (image.RawFormat.Equals(ImageFormat.Gif))
+                    {
+                        this.frames = new List<Image>();
+                        this.fileInfo = new FileInfo(filePath);
+
+                        if (ImageAnimator.CanAnimate(image))
+                        {
+                            //Get frames
+                            var dimension = new FrameDimension(image.FrameDimensionsList[0]);
+                            int frameCount = image.GetFrameCount(dimension);
+
+                            int index = 0;
+                            int duration = 0;
+                            for (int i = 0; i < frameCount; i++)
+                            {
+                                image.SelectActiveFrame(dimension, i);
+                                var frame = image.Clone() as Image;
+                                frames.Add(frame);
+
+                                var delay = BitConverter.ToInt32(image.GetPropertyItem(20736).Value, index) * 10;
+                                duration += (delay < 100 ? 100 : delay);
+
+                                index += 4;
+                            }
+
+                            this.animationDuration = TimeSpan.FromMilliseconds(duration);
+                            this.animated = true;
+                            this.loop = BitConverter.ToInt16(image.GetPropertyItem(20737).Value, 0) != 1;
+                        }
+                        else
+                        {
+                            this.frames.Add(image.Clone() as Image);
+                        }
+
+                    }
+                    else
+                    {
+                        throw new FormatException("Not valid GIF image format");
+                    }
+                }
+
+            }
+        }
+        #endregion
+    }
+}
+
--- IoT.Windows.Prism/ViewModels/MainViewModel.cs
+++ IoT.Windows.Prism/ViewModels/MainViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using IoT.Windows.Prism.Services;
-using Prism.Commands;
 using Prism.Windows.Mvvm;
 using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -15,28 +15,42 @@ namespace IoT.Windows.Prism.ViewModels
 
         public MainViewModel()
         {
-            StartMatrixCommand = new DelegateCommand(StartLedMatrix());
+            StartLedMatrix();
+            //StartMatrixCommand = new DelegateCommand(StartLedMatrix());
         }
 
-        private Action StartLedMatrix()
+        private async Task StartLedMatrix()
         {
-            return async () =>
+            //return async () =>
             {
-                var gif = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/jake.gif"));
+                var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/StoreLogo.png"));
 
+                var inputStream = await file.OpenSequentialReadAsync();
+
+                var binaryReader = new BinaryReader(inputStream.AsStreamForRead());
+
+                var buffer = new byte[14];
+                binaryReader.Read(buffer, 0, 14);
+                
                 var ledMatrix = new LedMatrix();
                 await ledMatrix.InitializeAsync(64);
+                //ledMatrix.Clean();
+                //ledMatrix.Write(0, 100, 00, 0);
+                //ledMatrix.Write(8, 0, 100, 0);
+                //ledMatrix.Write(16, 0, 0, 100);
+                //ledMatrix.RefreshLeds();
                 Random random = new Random();
                 while (true)
                 {
-                    ledMatrix.Write(random.Next(0, 63), (byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255));
-                    ledMatrix.Write(random.Next(0, 63), (byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255));
-                    ledMatrix.Write(random.Next(0, 63), (byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255));
-                    ledMatrix.Write(random.Next(0, 63), (byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255));
+                    ledMatrix.Clean();
+                    ledMatrix.Write(random.Next(0, 63), (byte)random.Next(0, 50), (byte)random.Next(0, 50), (byte)random.Next(0, 50));
+                    ledMatrix.Write(random.Next(0, 63), (byte)random.Next(0, 50), (byte)random.Next(0, 50), (byte)random.Next(0, 50));
+                    ledMatrix.Write(random.Next(0, 63), (byte)random.Next(0, 50), (byte)random.Next(0, 50), (byte)random.Next(0, 50));
+                    ledMatrix.Write(random.Next(0, 63), (byte)random.Next(0, 50), (byte)random.Next(0, 50), (byte)random.Next(0, 50));
                     ledMatrix.RefreshLeds();
                     Thread.Sleep(1);
                 }
-            };
+            }
         }
     }
 }
